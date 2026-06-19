ebfbd63b0ed42bd03632e0a5d19a845b532cc195
TtitiO <ting_lin@sjtu.edu.cn>
2026-06-19 12:28:52 +0800

    doc: revise build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir -p build
 cd build
-cmake -DPython_EXECUTABLE=../.venv/bin/python ..
+cmake -DPython_EXECUTABLE=../../.venv/bin/python ..
 make -j$(nproc)
 cd ..
-uv pip install --no-build-isolation -e .
-PYTHONPATH=python python -m ramulator codegen
+../.venv/bin/python -m pip install --no-build-isolation -e .
+PYTHONPATH=python ../.venv/bin/python -m ramulator codegen
